8ca2505d975745dfebd4603f461f8368122cd873
ong-ar <reclusis@gmail.com>
2018-09-08 22:29:50 +0900

    타입스크립트 연습 : interface, object

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "sis",
+  gender: "male",
+  age: 22
 };
 
-console.log(sayHi("sis", 22, "male"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${
+    person.gender
+  }`;
+};
+
+console.log(sayHi(person));
 
 export {};
